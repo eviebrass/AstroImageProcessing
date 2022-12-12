62c50b794c7db20b3495c329d71a4a05bedadece
eviebrass <90005948+eviebrass@users.noreply.github.com>
2022-12-12 10:09:20 +0000

    Update test.py

--- test.py
+++ test.py
@@ -10,26 +10,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-#defining simple data sets for testing
-empty_test = np.zeros((4172,2135)) #empty
-same_val_test = np.ones((4172,2135)) #same value
-noise_test = np.random.rand(4172,2135) #noise
+# defining simple data sets for testing
+empty_test = np.zeros((4172,2135)) # empty
+same_val_test = np.ones((4172,2135)) # same value
+noise_test = np.random.rand(4172,2135) # noise
 
-#defining a function that outputs gaussain blob to simular Gaussian blobs
+# defining a function that outputs gaussain blob to simular Gaussian blobs
 def gauss_2d(size,x_offset,y_offset,sigma):
     x = np.arange(0,size)
     y = np.arange(0,size)
     xx,yy= np.meshgrid(x,y)
     xx= xx-x_offset
     yy= yy -y_offset
-    sigma = 0.6063   #used 1 arsec FWHM, convert to pixel then sd
+    sigma = 9.1085 # used 1 arsec FWHM, convert to pixel then sd
     power = (xx*xx) +(yy*yy)
     denom = 1/(2*np.pi*sigma*sigma)
     return denom* np.exp(-power/(2*sigma*sigma))  
 
-n_objects = 10 #number of objects  -1
+n_objects = 10 # number of objects  -1
 
-#generating random radii array
+# generating random radii array
 random.seed(20)
 radi_ran = [[random.randint(0,100) for i in range(n_objects)] for j in range(2)]
 test_obj = []
